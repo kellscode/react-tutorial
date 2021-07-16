de6f1e107072c6f3d6693a2adc18a3c037181aba
Kelly Lee <yl3267@cornell.edu>
2021-07-16 14:28:20 +0900

    Updates

---
+++ precache-manifest.553bf553f495ca7d7a3add00480fc97d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "749f4b8676448181c875",
+    "url": "/react-tutorial/static/css/main.eed95c27.chunk.css"
+  },
+  {
+    "revision": "749f4b8676448181c875",
+    "url": "/react-tutorial/static/js/main.6c7145f9.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  },
+  {
+    "revision": "472f472de4ff1afb7a7c",
+    "url": "/react-tutorial/static/js/2.65eeef47.chunk.js"
+  },
+  {
+    "revision": "69779ecb734d0f3b9fc2a39d384054ed",
+    "url": "/react-tutorial/index.html"
+  }
+];
